a5d475668cf95e74b742c76ccbac2eae6c097e33
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-06-21 12:52:59 +0200

    First view rendered

--- library/views.py
+++ library/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+# from django.core import serializers
+# from django.http import HttpResponse
+# from django.views.decorators.csrf import csrf_exempt
+# from json import loads, dumps
+
+
+def index(request):
+    return render(request, 'index.html')
